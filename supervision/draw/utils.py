807785a8206b56d15438ea2c462e6eac85a8897b
SkalskiP <piotr.skalski92@gmail.com>
2023-02-06 22:35:00 +0100

    - draw_text added

--- supervision/draw/utils.py
+++ supervision/draw/utils.py
@@ -4,6 +4,8 @@ import numpy as np
 from supervision.draw.color import Color
 from supervision.geometry.core import Point, Rect
 
+from typing import Optional
+
 
 def draw_line(
     scene: np.ndarray, start: Point, end: Point, color: Color, thickness: int = 2
@@ -45,11 +47,6 @@ def draw_rectangle(
 
     Returns:
         np.ndarray: The scene with the rectangle drawn on it
-
-    Example:
-        ```python
-        >>> # TODO: Add example
-        ```
     """
     cv2.rectangle(
         scene,
@@ -77,12 +74,6 @@ def draw_filled_rectangle(scene: np.ndarray, rect: Rect, color: Color) -> np.nda
 
     Returns:
         np.ndarray: The scene with the rectangle drawn on it
-
-    Example:
-        ```python
-        >>> # TODO: Add example
-        ```
-
     """
     cv2.rectangle(
         scene,
@@ -110,5 +101,71 @@ def draw_polygon(
     """
     cv2.polylines(
         scene, [polygon], isClosed=True, color=color.as_bgr(), thickness=thickness
+    )
+    return scene
+
+
+def draw_text(
+    scene: np.ndarray,
+    text: str,
+    text_anchor: Point,
+    text_color: Color = Color.black(),
+    text_scale: float = 0.5,
+    text_thickness: int = 1,
+    text_padding: int = 10,
+    text_font: int = cv2.FONT_HERSHEY_SIMPLEX,
+    background_color: Optional[Color] = None,
+) -> np.ndarray:
+    """
+    Draw text on a scene.
+
+    This function takes in a 2-dimensional numpy ndarray representing an image or scene, and draws text on it using OpenCV's putText function. The text is anchored at a specified Point, and its appearance can be customized using arguments such as color, scale, and font. An optional background color and padding can be specified to draw a rectangle behind the text.
+
+    Parameters:
+        scene (np.ndarray): A 2-dimensional numpy ndarray representing an image or scene.
+        text (str): The text to be drawn.
+        text_anchor (Point): The anchor point for the text, represented as a Point object with x and y attributes.
+        text_color (Color, optional): The color of the text. Defaults to black.
+        text_scale (float, optional): The scale of the text. Defaults to 0.5.
+        text_thickness (int, optional): The thickness of the text. Defaults to 1.
+        text_padding (int, optional): The amount of padding to add around the text when drawing a rectangle in the background. Defaults to 10.
+        text_font (int, optional): The font to use for the text. Defaults to cv2.FONT_HERSHEY_SIMPLEX.
+        background_color (Color, optional): The color of the background rectangle, if one is to be drawn. Defaults to None.
+
+    Returns:
+        np.ndarray: The input scene with the text drawn on it.
+
+    Examples:
+        ```python
+        >>> scene = np.zeros((100, 100, 3), dtype=np.uint8)
+        >>> text_anchor = Point(x=50, y=50)
+        >>> scene = draw_text(scene=scene, text="Hello, world!", text_anchor=text_anchor)
+        ```
+    """
+    text_width, text_height = cv2.getTextSize(
+        text=text,
+        fontFace=text_font,
+        fontScale=text_scale,
+        thickness=text_thickness,
+    )[0]
+    text_rect = Rect(
+        x=text_anchor.x - text_width // 2,
+        y=text_anchor.y - text_height // 2,
+        width=text_width,
+        height=text_height
+    ).pad(text_padding)
+
+    if background_color is not None:
+        scene = draw_filled_rectangle(scene=scene, rect=text_rect, color=background_color)
+
+    cv2.putText(
+        img=scene,
+        text=text,
+        org=(text_anchor.x - text_width // 2, text_anchor.y + text_height // 2),
+        fontFace=text_font,
+        fontScale=text_scale,
+        color=text_color.as_bgr(),
+        thickness=text_thickness,
+        lineType=cv2.LINE_AA,
     )
     return scene
