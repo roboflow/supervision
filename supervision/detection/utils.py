25c19b4422897a69cbf60addb4159ea03a9097e3
SkalskiP <piotr.skalski92@gmail.com>
2023-03-07 22:35:09 +0100

    👋 initial commit

--- supervision/detection/utils.py
+++ supervision/detection/utils.py
@@ -20,38 +20,75 @@ def generate_2d_mask(polygon: np.ndarray, resolution_wh: Tuple[int, int]) -> np.
     return mask
 
 
-def non_max_suppression(boxes: np.ndarray, scores: np.ndarray, threshold: float):
-    assert boxes.shape[0] == scores.shape[0]
-    ys1 = boxes[:, 0]
-    xs1 = boxes[:, 1]
-    ys2 = boxes[:, 2]
-    xs2 = boxes[:, 3]
+def box_iou_batch(boxes_true: np.ndarray, boxes_detection: np.ndarray) -> np.ndarray:
+    """
+    Compute Intersection over Union of two sets of bounding boxes - `boxes_true` and `boxes_detection`. Both sets of
+    boxes are expected to be in `(x_min, y_min, x_max, y_max)` format.
 
-    areas = (ys2 - ys1) * (xs2 - xs1)
-    scores_indexes = scores.argsort().tolist()
-    boxes_keep_index = []
-    while len(scores_indexes):
-        index = scores_indexes.pop()
-        boxes_keep_index.append(index)
-        if not len(scores_indexes):
-            break
-        iou = compute_iou(
-            boxes[index], boxes[scores_indexes], areas[index], areas[scores_indexes]
-        )
-        filtered_indexes = set((iou > threshold).nonzero()[0])
-        scores_indexes = [
-            v for (i, v) in enumerate(scores_indexes) if i not in filtered_indexes
-        ]
-    return np.array(boxes_keep_index)
+    Properties:
+        boxes_true (np.ndarray): 2D `np.ndarray` representing ground-truth boxes. `shape = (N, 4)` where N is number of true objects.
+        boxes_detection (np.ndarray): 2D `np.ndarray` representing detection boxes. `shape = (M, 4)` where M is number of detected objects.
+
+    Returns:
+        np.ndarray: Pairwise IoU of boxes from `boxes_true` and `boxes_detection`. `shape = (N, M)` where N is number of true objects and M is number of detected objects.
+    """
+
+    def box_area(box):
+        return (box[2] - box[0]) * (box[3] - box[1])
+
+    area_true = box_area(boxes_true.T)
+    area_detection = box_area(boxes_detection.T)
+
+    top_left = np.maximum(boxes_true[:, None, :2], boxes_detection[:, :2])
+    bottom_right = np.minimum(boxes_true[:, None, 2:], boxes_detection[:, 2:])
+
+    area_inter = np.prod(np.clip(bottom_right - top_left, a_min=0, a_max=None), 2)
+    return area_inter / (area_true[:, None] + area_detection - area_inter)
 
 
-def compute_iou(box, boxes, box_area, boxes_area):
-    assert boxes.shape[0] == boxes_area.shape[0]
-    ys1 = np.maximum(box[0], boxes[:, 0])
-    xs1 = np.maximum(box[1], boxes[:, 1])
-    ys2 = np.minimum(box[2], boxes[:, 2])
-    xs2 = np.minimum(box[3], boxes[:, 3])
-    intersections = np.maximum(ys2 - ys1, 0) * np.maximum(xs2 - xs1, 0)
-    unions = box_area + boxes_area - intersections
-    iou = intersections / unions
-    return iou
+def non_max_suppression(
+    predictions: np.ndarray, iou_threshold: float = 0.5
+) -> np.ndarray:
+    """
+    Perform non-maximum suppression on object detection predictions.
+
+    Args:
+        predictions (np.ndarray): An array of object detection predictions in the format of (x_min, y_min, x_max, y_max, score) or (x_min, y_min, x_max, y_max, score, class).
+        iou_threshold (float, optional): The intersection-over-union threshold to use for non-maximum suppression. Defaults to 0.5.
+
+    Returns:
+        np.ndarray: A boolean array indicating which predictions to keep after non-maximum suppression.
+
+    Raises:
+        AssertionError: If `iou_threshold` is not within the closed range from 0 to 1.
+    """
+    assert 0 <= iou_threshold <= 1, (
+        f"Value of `iou_threshold` must be in the closed range from 0 to 1, "
+        f"{iou_threshold} given."
+    )
+    rows, columns = predictions.shape
+
+    # add column #5 - category filled with zeros for agnostic nms
+    if columns == 5:
+        predictions = np.c_[predictions, np.zeros(rows)]
+
+    # sort predictions column #4 - score
+    sort_index = np.flip(predictions[:, 4].argsort())
+    predictions = predictions[sort_index]
+
+    boxes = predictions[:, :4]
+    categories = predictions[:, 5]
+    ious = box_iou_batch(boxes, boxes)
+    ious = ious - np.eye(rows)
+
+    keep = np.ones(rows, dtype=bool)
+
+    for index, (iou, category) in enumerate(zip(ious, categories)):
+        if not keep[index]:
+            continue
+
+        # drop detections with iou > iou_threshold and same category as current detections
+        condition = (iou > iou_threshold) & (categories == category)
+        keep = keep & ~condition
+
+    return keep[sort_index.argsort()]
