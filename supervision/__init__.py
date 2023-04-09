511375f24b4daffab9ce35befeafe13d15f01554
SkalskiP <piotr.skalski92@gmail.com>
2023-04-09 23:05:56 +0200

    😷 Top level import of MaskAnnotator

--- supervision/__init__.py
+++ supervision/__init__.py
@@ -1,7 +1,7 @@
 __version__ = "0.4.0"
 
 from supervision.annotation.voc import detections_to_voc_xml
-from supervision.detection.annotate import BoxAnnotator
+from supervision.detection.annotate import BoxAnnotator, MaskAnnotator
 from supervision.detection.core import Detections
 from supervision.detection.line_counter import LineZone, LineZoneAnnotator
 from supervision.detection.polygon_zone import PolygonZone, PolygonZoneAnnotator
