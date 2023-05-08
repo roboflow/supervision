1bebbb14a6c97a1c0cdb14a0a17d824730f7c5c9
SkalskiP <piotr.skalski92@gmail.com>
2023-05-08 13:51:49 +0200

    yolo_annotations_to_detections function full implementation

--- supervision/dataset/formats/yolo.py
+++ supervision/dataset/formats/yolo.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from typing import List, Tuple
 
-from supervision import polygon_to_mask
+from supervision.detection.utils import polygon_to_mask, polygon_to_xyxy
 from supervision.detection.core import Detections
 
 
@@ -26,7 +26,7 @@ def box_to_polygon(box: np.ndarray) -> np.ndarray:
 
 
 def parse_polygon(values: List[str]) -> np.ndarray:
-    pass
+    return np.array(values, dtype=np.float32).reshape(-1, 2)
 
 
 def polygons_to_masks(polygons: List[np.ndarray], resolution_wh: Tuple[int, int]) -> np.ndarray:
@@ -40,42 +40,41 @@ def polygons_to_masks(polygons: List[np.ndarray], resolution_wh: Tuple[int, int]
 def yolo_annotations_to_detections(
     lines: List[str],
     resolution_wh: Tuple[int, int],
-    force_segmentations: bool
+    with_masks: bool
 ) -> Detections:
     if len(lines) == 0:
         return Detections.empty()
-    class_id, relative_xyxy, relative_polygons = [], [], []
+
+    class_id, relative_xyxy, relative_polygon = [], [], []
     w, h = resolution_wh
     for line in lines:
         values = line.split()
         class_id.append(int(values[0]))
-        if len(values) == 5 and not force_segmentations:
+        if len(values) == 5:
             box = parse_box(values=values[1:])
             relative_xyxy.append(box)
-        elif len(values) == 5 and force_segmentations:
-            box = parse_box(values=values[1:])
-            relative_xyxy.append(box)
-            relative_polygons.append(box_to_polygon(box=box))
-        elif len(values) > 5 and not force_segmentations:
-            pass
-        elif len(values) > 5 and force_segmentations:
-            pass
+            if with_masks:
+                relative_polygon.append(box_to_polygon(box=box))
+        elif len(values) > 5:
+            polygon = parse_polygon(values=values[1:])
+            relative_xyxy.append(polygon_to_xyxy(polygon=polygon))
+            if with_masks:
+                relative_polygon.append(polygon)
 
     class_id = np.array(class_id, dtype=int)
     relative_xyxy = np.array(relative_xyxy, dtype=np.float32)
     xyxy = relative_xyxy * np.array([w, h, w, h], dtype=np.float32)
 
-    if not force_segmentations:
+    if not with_masks:
         return Detections(class_id=class_id, xyxy=xyxy)
 
     polygons = [
-        (relative_polygon * np.array(resolution_wh)).astype(int)
-        for relative_polygon
-        in relative_polygons
+        (polygon * np.array(resolution_wh)).astype(int)
+        for polygon
+        in relative_polygon
     ]
     mask = polygons_to_masks(polygons=polygons, resolution_wh=resolution_wh)
     return Detections(class_id=class_id, xyxy=xyxy, mask=mask)
-
 
 
 def detections_to_yolo_annotations(detections: Detections, resolution_wh: Tuple[int, int]) -> List[str]:
