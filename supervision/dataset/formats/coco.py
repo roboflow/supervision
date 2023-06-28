66d29015d6c1bdddd7df465f155a29d56812e369
SkalskiP <piotr.skalski92@gmail.com>
2023-06-28 14:17:35 +0200

    🖤 make black happy

--- supervision/dataset/formats/coco.py
+++ supervision/dataset/formats/coco.py
@@ -15,8 +15,7 @@ from supervision.utils.file import read_json_file, save_json_file
 def coco_categories_to_classes(coco_categories: List[dict]) -> List[str]:
     return [
         category["name"]
-        for category
-        in sorted(coco_categories, key=lambda category: category["id"])
+        for category in sorted(coco_categories, key=lambda category: category["id"])
     ]
 
 
