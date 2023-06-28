194ff0aaad832ea1633c1f3053f55edc4ea29351
SkalskiP <piotr.skalski92@gmail.com>
2023-06-28 14:07:27 +0200

    small fix

--- supervision/dataset/formats/coco.py
+++ supervision/dataset/formats/coco.py
@@ -15,8 +15,8 @@ from supervision.utils.file import read_json_file, save_json_file
 def coco_categories_to_classes(coco_categories: List[dict]) -> List[str]:
     return [
         category["name"]
-        for category in sorted(coco_categories, key=lambda category: category["id"])
-        if category["supercategory"] != "none"
+        for category
+        in sorted(coco_categories, key=lambda category: category["id"])
     ]
 
 
