2b876bdd64e07298d0a514cc58d7fa6654c09ee2
kirilllzaitsev <kzaitsev88@gmail.com>
2023-07-07 22:29:17 +0200

    add from_matrix()

--- supervision/metrics/detection.py
+++ supervision/metrics/detection.py
@@ -18,6 +18,32 @@ class ConfusionMatrix:
     iou_threshold: float
 
     @classmethod
+    def from_matrix(
+        cls,
+        matrix,
+        conf_threshold: float,
+        iou_threshold: float,
+        classes: Optional[List[str]],
+    ) -> "ConfusionMatrix":
+        """
+        Create ConfusionMatrix from matrix.
+
+        Args:
+            matrix: confusion matrix.
+            classes:  all known classes.
+            conf_threshold:  detection confidence threshold between 0 and 1. Detections with lower confidence will be excluded.
+            iou_threshold:  detection iou  threshold between 0 and 1. Detections with lower iou will be classified as FP.
+        """
+        classes = classes if classes is not None else [str(x) for x in (range(len(matrix)))]
+        return cls(
+            matrix=matrix,
+            classes=classes,
+            num_classes=len(classes),
+            conf_threshold=conf_threshold,
+            iou_threshold=iou_threshold,
+        )
+
+    @classmethod
     def from_detections(
         cls,
         predictions: List[sv.Detections],
