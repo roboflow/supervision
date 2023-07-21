54524a4a1ffaf0489ac09626fb07cad5b78e626a
kirilllzaitsev <kzaitsev88@gmail.com>
2023-07-21 11:43:57 +0200

    drop docstring in _drop_extra_matches

--- supervision/metrics/detection.py
+++ supervision/metrics/detection.py
@@ -296,10 +296,6 @@ class ConfusionMatrix:
 
     @staticmethod
     def _drop_extra_matches(matches: np.ndarray) -> np.ndarray:
-        """
-        Deduplicate matches. If there are multiple matches for the same true or predicted box,
-        only the one with the highest IoU is kept.
-        """
         if matches.shape[0] > 0:
             matches = matches[matches[:, 2].argsort()[::-1]]
             matches = matches[np.unique(matches[:, 1], return_index=True)[1]]
