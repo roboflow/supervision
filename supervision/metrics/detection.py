4b38634c6365e6f7465fd648fbf1c70ec6884a9a
kirilllzaitsev <kzaitsev88@gmail.com>
2023-07-21 12:34:43 +0200

    add comments to _drop_extra_matches

--- supervision/metrics/detection.py
+++ supervision/metrics/detection.py
@@ -297,7 +297,10 @@ class ConfusionMatrix:
     @staticmethod
     def _drop_extra_matches(matches: np.ndarray) -> np.ndarray:
         if matches.shape[0] > 0:
+            # sort by IoU
             matches = matches[matches[:, 2].argsort()[::-1]]
+            # If there are multiple matches for the same true or predicted box,
+            # only the one with the highest IoU is kept.
             matches = matches[np.unique(matches[:, 1], return_index=True)[1]]
             matches = matches[matches[:, 2].argsort()[::-1]]
             matches = matches[np.unique(matches[:, 0], return_index=True)[1]]
