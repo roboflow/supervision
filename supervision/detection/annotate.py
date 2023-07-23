8066125f03d5434b1b2eddeaf4d706faba184729
SkalskiP <piotr.skalski92@gmail.com>
2023-07-23 19:43:51 +0200

    🧹‍💨 clean up

--- supervision/detection/annotate.py
+++ supervision/detection/annotate.py
@@ -2,7 +2,6 @@ from typing import List, Optional, Union
 
 import cv2
 import numpy as np
-from PIL import Image, ImageDraw, ImageFont
 
 from supervision.detection.core import Detections
 from supervision.draw.color import Color, ColorPalette
