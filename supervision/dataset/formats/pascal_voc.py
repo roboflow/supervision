8afd54a1cc9422a17e8ec0e92951519f6203e1e2
Mayank Agarwal <mayankagarwal44442@gmail.com>
2023-07-24 18:23:06 +0530

    format code

--- supervision/dataset/formats/pascal_voc.py
+++ supervision/dataset/formats/pascal_voc.py
@@ -146,10 +146,10 @@ def load_pascal_voc_annotations(
 
         # Correction for VOC XML format as bounding boxes in VOC XML start at (1,1) not (0,0). Refer:
         # https://github.com/roboflow/supervision/issues/144
-        x1 = int(bbox.find("xmin").text)-1
-        y1 = int(bbox.find("ymin").text)-1
-        x2 = int(bbox.find("xmax").text)-1
-        y2 = int(bbox.find("ymax").text)-1
+        x1 = int(bbox.find("xmin").text) - 1
+        y1 = int(bbox.find("ymin").text) - 1
+        x2 = int(bbox.find("xmax").text) - 1
+        y2 = int(bbox.find("ymax").text) - 1
 
         xyxy.append([x1, y1, x2, y2])
 
