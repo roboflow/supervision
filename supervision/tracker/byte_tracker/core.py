867dd78f58e57a86c538bf9d1265e2c518833491
SkalskiP <piotr.skalski92@gmail.com>
2023-08-07 17:51:08 +0200

    📝 small ByteTrack docs updates

--- supervision/tracker/byte_tracker/core.py
+++ supervision/tracker/byte_tracker/core.py
@@ -206,22 +206,22 @@ class ByteTrack:
             >>> byte_tracker = sv.ByteTrack()
             >>> annotator = sv.BoxAnnotator()
 
-            def callback(frame: np.ndarray, index: int) -> np.ndarray:
-                results = model(frame)[0]
-                detections = sv.Detections.from_yolov8(results)
-                detections = byte_tracker.update_from_detections(detections=detections)
-                labels = [
-                    f"#{tracker_id} {model.model.names[class_id]} {confidence:0.2f}"
-                    for _, _, confidence, class_id, tracker_id
-                    in detections
-                ]
-                return annotator.annotate(scene=frame.copy(), detections=detections, labels=labels)
+            >>> def callback(frame: np.ndarray, index: int) -> np.ndarray:
+            ...     results = model(frame)[0]
+            ...     detections = sv.Detections.from_yolov8(results)
+            ...     detections = byte_tracker.update_from_detections(detections=detections)
+            ...     labels = [
+            ...         f"#{tracker_id} {model.model.names[class_id]} {confidence:0.2f}"
+            ...         for _, _, confidence, class_id, tracker_id
+            ...         in detections
+            ...     ]
+            ...     return annotator.annotate(scene=frame.copy(), detections=detections, labels=labels)
 
-            sv.process_video(
-                source_path='...',
-                target_path='...',
-                callback=callback
-            )
+            >>> sv.process_video(
+            ...     source_path='...',
+            ...     target_path='...',
+            ...     callback=callback
+            ... )
             ```
         """
 
