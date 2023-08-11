04629cd7f4ce996275cc46e2af2a4cf3c2a9bbda
Hardik Dava <hardik1901dava@gmail.com>
2023-08-11 14:07:14 +0200

    initial commit

--- supervision/__init__.py
+++ supervision/__init__.py
@@ -34,6 +34,7 @@ from supervision.metrics.detection import ConfusionMatrix, MeanAveragePrecision
 from supervision.utils.file import list_files_with_extensions
 from supervision.utils.image import ImageSink, crop
 from supervision.utils.notebook import plot_image, plot_images_grid
+from supervision.utils.fps import FpsMonitor
 from supervision.utils.video import (
     VideoInfo,
     VideoSink,
