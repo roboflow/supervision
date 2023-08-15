7bd6a107ce0bd4907dc56d0590525fc2b0f388bb
Mayank Agarwal <mayankagarwal44442@gmail.com>
2023-08-15 11:02:02 +0530

    fix pre-commit-ci flake8 test

--- supervision/classification/core.py
+++ supervision/classification/core.py
@@ -79,8 +79,8 @@ class Classifications:
     @classmethod
     def from_ultralytics(cls, ultralytics_results) -> Classifications:
         """
-        Creates a Classifications instance from a (https://github.com/ultralytics/ultralytics)
-        inference result.
+        Creates a Classifications instance from a
+        (https://github.com/ultralytics/ultralytics) inference result.
 
         Args:
             ultralytics_results (ultralytics.engine.results.Results):
