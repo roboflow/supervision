bba63dd571d965dc42a902d36ee1cbcb2d7ff15b
Derrick Mwiti <mwitiderrick@gmail.com>
2023-08-18 10:29:26 +0000

    update docstring

--- supervision/detection/core.py
+++ supervision/detection/core.py
@@ -313,8 +313,10 @@ class Detections:
 
             >>> yolo_pipeline = Pipeline.create(
             ...     task="yolo",
-            ...     model_path="zoo:cv/detection/yolov5-l/pytorch/ultralytics/coco/pruned80_quant-none")
-            >>> pipeline_outputs = yolo_pipeline(SOURCE_IMAGE_PATH, iou_thres=0.6, conf_thres=0.001)
+            ...     model_path="zoo:cv/detection/yolov5-l/pytorch/
+            ...        ultralytics/coco/pruned80_quant-none")
+            >>> pipeline_outputs = yolo_pipeline(SOURCE_IMAGE_PATH,
+            ...                         iou_thres=0.6, conf_thres=0.001)
             >>> result = list(pipeline_outputs.boxes[0])
             >>> detections = sv.Detections.from_yolo_nas(result)
             ```
