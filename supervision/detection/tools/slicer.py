a8eb38198c0ce06306c24aac328dcb2df29630a9
SkalskiP <piotr.skalski92@gmail.com>
2023-08-30 15:08:07 +0200

    slicer refactor

--- supervision/detection/tools/slicer.py
+++ supervision/detection/tools/slicer.py
@@ -2,11 +2,24 @@ from typing import Callable, Optional, Tuple
 
 import numpy as np
 
+from supervision import crop_image
 from supervision.detection.core import Detections, validate_inference_callback
 from supervision.detection.utils import move_boxes
 
 
-class Slicer:
+def move_detections(detections: Detections, offset: np.array) -> Detections:
+    """
+    Args:
+        detections (sv.Detections): Detections object to be moved.
+        offset (np.array): An array of shape `(2,)` containing offset values in format is `[dx, dy]`.
+    Returns:
+        (sv.Detections) repositioned Detections object.
+    """
+    detections.xyxy = move_boxes(xyxy=detections.xyxy, offset=offset)
+    return detections
+
+
+class InferenceSlicer:
     """
     Slicing inference(SAHI) method for small target detection.
     """
@@ -14,114 +27,68 @@ class Slicer:
     def __init__(
         self,
         callback: Callable[[np.ndarray], Detections],
-        sliced_width: Optional[int] = 320,
-        sliced_height: Optional[int] = 320,
-        overlap_width_ratio: Optional[float] = 0.2,
-        overlap_height_ratio: Optional[float] = 0.2,
+        slice_wh: Tuple[int, int] = (320, 320),
+        overlap_ratio_wh: Tuple[float, float] = (0.2, 0.2),
         iou_threshold: Optional[float] = 0.5,
     ):
-        """
-        Args:
-            callback (Callable): model callback method which returns detections as sv.Detections
-            sliced_width (int): width of each slice
-            sliced_height (int): height of each slice
-            overlap_width_ratio (float): Fractional overlap in width of each
-                                        slice (e.g. an overlap of 0.2 for a slice of size 100 yields an
-                                        overlap of 20 pixels). Default 0.2.
-            overlap_height_ratio (float): Fractional overlap in height of each
-                                        slice (e.g. an overlap of 0.2 for a slice of size 100 yields an
-                                        overlap of 20 pixels). Default 0.2.
-            iou_threshold (float): non-maximum suppression iou threshold to remove overlapping detections
-        """
-        self.siced_width = sliced_width
-        self.sliced_height = sliced_height
-        self.overlap_width_ratio = overlap_width_ratio
-        self.overlap_height_ratio = overlap_height_ratio
+        self.slice_wh = slice_wh
+        self.overlap_ratio_wh = overlap_ratio_wh
         self.iou_threshold = iou_threshold
         self.callback = callback
         validate_inference_callback(callback=callback)
 
     def __call__(self, image: np.ndarray) -> Detections:
-        """
-
-        Args:
-            image (np.ndarray):
-
-        Returns:
-            sv.Detections
-
-             Example:
-            ```python
-            >>> import supervision as sv
-            >>> from ultralytics import YOLO
-
-            >>> dataset = sv.DetectionDataset.from_yolo(...)
-
-            >>> model = YOLO(...)
-            >>> def callback(slice: np.ndarray) -> sv.Detections:
-            ...     result = model(slice)[0]
-            ...     return sv.Detections.from_ultralytics(result)
-
-            >>> slicer = sv.Slicer(
-            ...     callback = callback
-            ... )
-
-            >>> detections = slicer(image)
-            ```
-        """
-        detections = []
-        image_height, image_width, _ = image.shape
-        offsets = self._offset_generation(
-            image_width=image_width, image_height=image_height
+        detections_list = []
+        resolution_wh = (image.shape[1], image.shape[0])
+        offsets = self._generate_offset(
+            resolution_wh=resolution_wh,
+            slice_wh=self.slice_wh,
+            overlap_ratio_wh=self.overlap_ratio_wh,
         )
 
         for offset in offsets:
-            slice = image[offset[1] : offset[3], offset[0] : offset[2]]
-            det = self.callback(slice)
-            det = self._reposition_detections(detection=det, offset=offset)
-            detections.append(det)
-        detection = Detections.merge(detections_list=detections).with_nms(
+            image_slice = crop_image(image=image, xyxy=offset)
+            detections = self.callback(image_slice)
+            detections = move_detections(detections=detections, offset=offset)
+            detections_list.append(detections)
+        return Detections.merge(detections_list=detections_list).with_nms(
             threshold=self.iou_threshold
         )
-        return detection
-
-    def _offset_generation(self, image_width: int, image_height: int) -> np.ndarray:
-        """
-        Args:
-            image_width (int): width of the input image
-            image_height (int): height of the input image
-
-        Returns:
-            list of slice locations according to slicer parameters
-        """
-        width_stride = self.siced_width - int(
-            self.overlap_width_ratio * self.siced_width
-        )
-        height_stride = self.sliced_height - int(
-            self.overlap_height_ratio * self.sliced_height
-        )
-        offsets = []
-        for h in range(0, image_height, height_stride):
-            for w in range(0, image_width, width_stride):
-                xmin = w
-                ymin = h
-                xmax = min(image_width, w + self.siced_width)
-                ymax = min(image_height, h + self.sliced_height)
-                offsets.append([xmin, ymin, xmax, ymax])
-        offsets = np.asarray(offsets)
-        return offsets
 
     @staticmethod
-    def _reposition_detections(detection: Detections, offset: np.array) -> Detections:
+    def _generate_offset(
+        resolution_wh: Tuple[int, int],
+        slice_wh: Tuple[int, int],
+        overlap_ratio_wh: Tuple[float, float],
+    ) -> np.ndarray:
         """
+        Generate offset coordinates for slicing an image based on the given resolution, slice dimensions, and overlap ratios.
+
         Args:
-            detection (np.ndarray): result of model inference of the slice
-            slice_location (Tuple[int, int, int, int]): slice location at which inference was performed
+            resolution_wh (Tuple[int, int]): A tuple representing the width and height of the image to be sliced.
+            slice_wh (Tuple[int, int]): A tuple representing the desired width and height of each slice.
+            overlap_ratio_wh (Tuple[float, float]): A tuple representing the desired overlap ratio for width and height between consecutive slices. Each value should be in the range [0, 1), where 0 means no overlap and a value close to 1 means high overlap.
+
         Returns:
-            (sv.Detections) repositioned detections result based on original image
+            np.ndarray: An array of shape `(n, 4)` containing coordinates for each slice in the format `[xmin, ymin, xmax, ymax]`.
+
+        Note:
+            The function ensures that slices do not exceed the boundaries of the original image. As a result, the final slices in the row and column dimensions might be smaller than the specified slice dimensions if the image's width or height is not a multiple of the slice's width or height minus the overlap.
         """
-        if len(detection) == 0:
-            return detection
-        xyxy = move_boxes(boxes=detection.xyxy, offset=offset)
-        detection.xyxy = xyxy
-        return detection
+        slice_width, slice_height = slice_wh
+        image_width, image_height = resolution_wh
+        overlap_ratio_width, overlap_ratio_height = overlap_ratio_wh
+
+        width_stride = slice_width - int(overlap_ratio_width * slice_width)
+        height_stride = slice_height - int(overlap_ratio_height * slice_height)
+
+        ws = np.arange(0, image_width, width_stride)
+        hs = np.arange(0, image_height, height_stride)
+
+        xmin, ymin = np.meshgrid(ws, hs)
+        xmax = np.clip(xmin + slice_width, 0, image_width)
+        ymax = np.clip(ymin + slice_height, 0, image_height)
+
+        offsets = np.stack([xmin, ymin, xmax, ymax], axis=-1).reshape(-1, 4)
+
+        return offsets
