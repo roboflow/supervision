89c1b63979d43f8ed651a9701ca8333034d0bc07
SkalskiP <piotr.skalski92@gmail.com>
2023-10-06 19:24:10 +0200

    Optimize color assignment in mask annotation

--- supervision/annotators/core.py
+++ supervision/annotators/core.py
@@ -148,8 +148,7 @@ class MaskAnnotator(BaseAnnotator):
             color = resolve_color(color=self.color, idx=idx)
             mask = detections.mask[detection_idx]
             colored_mask = np.zeros_like(scene, dtype=np.uint8)
-            color_bgr = color.as_bgr()
-            colored_mask[:] = color_bgr
+            colored_mask[:] = color.as_bgr()
             scene[mask] = cv2.addWeighted(
                 colored_mask, self.opacity, scene, 1 - self.opacity, 0
             )[mask]
