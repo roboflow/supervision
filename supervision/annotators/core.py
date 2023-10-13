3dbbcf79ae3d719bf1577208b09d54c1ff582bbf
Rajarshi Misra <72802594+Rajarshi-Misra@users.noreply.github.com>
2023-10-13 22:55:41 +0530

    Final

--- supervision/annotators/core.py
+++ supervision/annotators/core.py
@@ -754,7 +754,7 @@ class BlurAnnotator(BaseAnnotator):
             >>> detections = sv.Detections(...)
 
             >>> blur_annotator = sv.BlurAnnotator()
-            >>> annotated_frame = blur_annotator.annotate(
+            >>> annotated_frame = circle_annotator.annotate(
             ...     scene=image.copy(),
             ...     detections=detections
             ... )
