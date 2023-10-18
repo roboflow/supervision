57ba059f1c2a1016ccf5a1a122c1aa8c26e8e745
SkalskiP <piotr.skalski92@gmail.com>
2023-10-18 22:45:58 +0200

    test

--- supervision/draw/utils.py
+++ supervision/draw/utils.py
@@ -172,64 +172,44 @@ def draw_text(
 
 
 def draw_image(
-    scene: np.ndarray, image: Union[str, np.ndarray], opacity: float, rect: Rect
+        scene: np.ndarray, image: Union[str, np.ndarray], opacity: float, rect: Rect
 ) -> np.ndarray:
     """
     Draws an image onto a given scene with specified opacity and dimensions.
-
-    Args:
-        scene (np.ndarray): The background image onto which the image will be drawn.
-        image (Union[str, np.ndarray]): The image to be drawn.
-            Can be either a file path or a NumPy array.
-        opacity (float): The opacity level of the image to be drawn,
-            ranging from 0.0 to 1.0.
-        rect (Rect): A Rect object specifying the dimensions and
-            position where the image will be drawn.
-
-    Returns:
-        np.ndarray: The scene with the image drawn onto it.
-
-    Example:
-        ```python
-        >>> scene = np.zeros((400, 400, 3), dtype=np.uint8)
-        >>> image_path = "path/to/image.jpg"
-        >>> opacity = 0.5
-        >>> rect = Rect(x=50, y=50, width=200, height=200)
-        >>> new_scene = draw_image(scene, image_path, opacity, rect)
-        ```
     """
+    # Input checks
+    assert 0.0 <= opacity <= 1.0, "Opacity has to be between 0.0 and 1.0."
+    assert rect.x >= 0 and rect.y >= 0, "Top left coordinates of the rect must be positive."
+    assert rect.width > 0 and rect.height > 0, "Rect dimensions should be positive."
+    assert rect.x + rect.width <= scene.shape[1] and rect.y + rect.height <= \
+           scene.shape[0], "Image exceeds scene bounds."
+
+    # Read image if a path is provided
     if isinstance(image, str):
-        assert os.path.exists(image), f'The specified path ("{image}") does not exist.'
+        assert os.path.exists(image), f"The specified path '{image}' does not exist."
         image = cv2.imread(image, cv2.IMREAD_UNCHANGED)
 
-    assert 0.0 <= opacity <= 1.0, "The opacity has to be between 0.0 and 1.0."
-
-    assert (
-        rect.x >= 0 and rect.y >= 0
-    ), "The top left coordinates of the rectangle have to be positive."
-    assert (
-        rect.x + rect.width <= scene.shape[1] and rect.y + rect.height <= scene.shape[0]
-    ), "The image you are trying to draw exceeds the bounds of the scene."
-
+    # Resize the image
     image = cv2.resize(image, (rect.width, rect.height))
 
+    # If image has an alpha channel, extract it
+    alpha_channel = None
     if image.shape[2] == 4:
-        b, g, r, a = cv2.split(image)
-        b = cv2.bitwise_and(b, b, mask=a)
-        g = cv2.bitwise_and(g, g, mask=a)
-        r = cv2.bitwise_and(r, r, mask=a)
-        image = cv2.merge([b, g, r, a])
-        del b, g, r, a
+        alpha_channel = image[:, :, 3]
+        image = cv2.cvtColor(image, cv2.COLOR_BGRA2BGR)
 
-        if scene.shape[2] == 3:
-            scene = np.dstack([scene, np.ones(scene.shape[:2], dtype=np.uint8) * 255])
+    # Apply opacity to the alpha channel
+    if alpha_channel is None:
+        alpha_channel = np.ones(image.shape[:2], dtype=image.dtype) * 255
+    alpha_channel = cv2.convertScaleAbs(alpha_channel * opacity)
 
-    scene_h, scene_w, channels = scene.shape[:3]
-    water_h, water_w = image.shape[:2]
+    # Get ROI from the scene
+    scene_roi = scene[rect.y:rect.y + rect.height, rect.x:rect.x + rect.width]
 
-    overlay = np.zeros((scene_h, scene_w, channels), dtype="uint8")
-    overlay[rect.y : rect.y + water_h, rect.x : rect.x + water_w] = image
+    # Blend the image and the ROI
+    blended_roi = cv2.addWeighted(scene_roi, 1 - opacity, image, opacity, 0)
 
-    cv2.addWeighted(overlay, opacity, scene, 1.0, 0, scene)
+    # Apply the blended ROI to the scene
+    scene[rect.y:rect.y + rect.height, rect.x:rect.x + rect.width] = blended_roi
 
-    return scene[:, :, :3]
+    return scene
