9f9e205a3309ea132e07f07a394e78f95b4873f8
Hitesh Kumar <hitesh.kumar61@yahoo.com>
2023-10-18 23:12:14 +0530

    remove docstring

--- supervision/detection/core.py
+++ supervision/detection/core.py
@@ -15,9 +15,6 @@ from supervision.geometry.core import Position
 
 
 def _validate_array(value: Any, n: int, name: str) -> None:
-    """
-    Checks if the array is either `None` or a numpy array with the first dimension of size `n`.
-    """
     is_valid = value is None or (isinstance(value, np.ndarray) and value.shape[0] == n)
     if not is_valid:
         raise ValueError(
