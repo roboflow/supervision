f987e4f7862c5fc1d1bd2fddbff72b57c33399b7
SkalskiP <piotr.skalski92@gmail.com>
2023-10-24 13:23:05 +0200

    Refactor video assets enum and variable names
    Renamed enum and variables to improve clarity for VideoAssets in asset_list.py. For instance, MILK_1 is updated to MILK_BOTTLED_PLANT and PEOPLE_WALKING_BW to PEOPLE_WALKING. Also, the VIDEO_ASSETS dictionary type hint updated to Dict. Additionally, a new method 'list' added to class VideoAssets that returns a list of all enum values.

--- supervision/assets/asset_list.py
+++ supervision/assets/asset_list.py
@@ -1,19 +1,25 @@
 from enum import Enum
 
+from typing import Tuple, Dict
+
 BASE_VIDEO_URL = "https://media.roboflow.com/supervision/video-examples/"
 
 
 class VideoAssets(Enum):
     VEHICLES = "vehicles.mp4"
-    MILK_1 = "milk-video-1.mp4"
+    MILK_BOTTLING_PLANT = "milk-bottling-plant.mp4"
     VEHICLES_2 = "vehicles-2.mp4"
     GROCERY_STORE = "grocery-store.mp4"
     SUBWAY = "subway.mp4"
     MARKET_SQUARE = "market-square.mp4"
-    PEOPLE_WALKING_BW = "people-walking-bw.mp4"
+    PEOPLE_WALKING = "people-walking.mp4"
+
+    @classmethod
+    def list(cls):
+        return list(map(lambda c: c.value, cls))
 
 
-VIDEO_ASSETS: dict[str : tuple[str]] = {
+VIDEO_ASSETS: Dict[str, Tuple[str, str]] = {
     VideoAssets.VEHICLES.value: (
         f"{BASE_VIDEO_URL}{VideoAssets.VEHICLES.value}",
         "8155ff4e4de08cfa25f39de96483f918",
@@ -22,8 +28,8 @@ VIDEO_ASSETS: dict[str : tuple[str]] = {
         f"{BASE_VIDEO_URL}{VideoAssets.VEHICLES_2.value}",
         "830af6fba21ffbf14867a7fea595937b",
     ),
-    VideoAssets.MILK_1.value: (
-        f"{BASE_VIDEO_URL}{VideoAssets.MILK_1.value}",
+    VideoAssets.MILK_BOTTLING_PLANT.value: (
+        f"{BASE_VIDEO_URL}{VideoAssets.MILK_BOTTLING_PLANT.value}",
         "9e8fb6e883f842a38b3d34267290bdc7",
     ),
     VideoAssets.GROCERY_STORE.value: (
@@ -38,8 +44,8 @@ VIDEO_ASSETS: dict[str : tuple[str]] = {
         f"{BASE_VIDEO_URL}{VideoAssets.MARKET_SQUARE.value}",
         "859179bf4a21f80a8baabfdb2ed716dc",
     ),
-    VideoAssets.PEOPLE_WALKING_BW.value: (
-        f"{BASE_VIDEO_URL}{VideoAssets.PEOPLE_WALKING_BW.value}",
+    VideoAssets.PEOPLE_WALKING.value: (
+        f"{BASE_VIDEO_URL}{VideoAssets.PEOPLE_WALKING.value}",
         "0574c053c8686c3f1dc0aa3743e45cb9",
     ),
 }
