6e039d7ebda2750a2a8505cbf0c37b480c344322
Piotr Skalski <piotr.skalski92@gmail.com>
2023-10-31 17:30:02 +0100

    Update init_kapa_widget.js

--- docs/javascript/init_kapa_widget.js
+++ docs/javascript/init_kapa_widget.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
     script.setAttribute("data-website-id", "e83c5c60-2968-410b-a2da-08fb104f23df");
     script.setAttribute("data-project-name", "Roboflow");
     script.setAttribute("data-project-color", "#6405C9");
-    script.setAttribute("data-project-logo", "https:/media.roboflow.com/chat.png");
+    script.setAttribute("data-project-logo", "https://media.roboflow.com/chat.png");
     script.async = true;
     document.head.appendChild(script);
 });
