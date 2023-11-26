e6f0ae8514c20b8f19e95281eb9dfb6c618e3c02
SkalskiP <piotr.skalski92@gmail.com>
2023-11-26 11:25:59 +0100

    Add 'as_hex' method and corresponding test to Color class.

--- supervision/draw/color.py
+++ supervision/draw/color.py
@@ -68,8 +68,23 @@ class Color:
         color_hex = color_hex.lstrip("#")
         if len(color_hex) == 3:
             color_hex = "".join(c * 2 for c in color_hex)
-        r, g, b = (int(color_hex[i : i + 2], 16) for i in range(0, 6, 2))
+        r, g, b = (int(color_hex[i: i + 2], 16) for i in range(0, 6, 2))
         return cls(r, g, b)
+
+    def as_hex(self) -> str:
+        """
+        Converts the Color instance to a hex string.
+
+        Returns:
+            str: The hexadecimal color string.
+
+        Example:
+            ```
+            >>> Color(r=255, g=0, b=255).as_hex()
+            '#ff00ff'
+            ```
+        """
+        return f"#{self.r:02x}{self.g:02x}{self.b:02x}"
 
     def as_rgb(self) -> Tuple[int, int, int]:
         """
