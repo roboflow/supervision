e08e4ee3894d03ebeef49d3de9104412fe18b6f1
SkalskiP <piotr.skalski92@gmail.com>
2023-12-04 17:05:49 +0100

    Rename calculate_dynamic_font_scale to calculate_dynamic_text_scale

--- supervision/__init__.py
+++ supervision/__init__.py
@@ -45,8 +45,8 @@ from supervision.detection.utils import (
 )
 from supervision.draw.color import Color, ColorPalette
 from supervision.draw.utils import (
-    calculate_dynamic_text_scale,
     calculate_dynamic_line_thickness,
+    calculate_dynamic_text_scale,
     draw_filled_rectangle,
     draw_image,
     draw_line,
