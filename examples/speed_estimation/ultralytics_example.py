c78354203d7bc5c211d3de30e72c0a1293a156e2
SkalskiP <piotr.skalski92@gmail.com>
2024-01-16 17:51:56 +0100

    ready for review

--- examples/speed_estimation/ultralytics_example.py
+++ examples/speed_estimation/ultralytics_example.py
@@ -23,12 +23,16 @@ TARGET = np.array(
 
 
 class ViewTransformer:
+
     def __init__(self, source: np.ndarray, target: np.ndarray) -> None:
         source = source.astype(np.float32)
         target = target.astype(np.float32)
         self.m = cv2.getPerspectiveTransform(source, target)
 
     def transform_points(self, points: np.ndarray) -> np.ndarray:
+        if points.size == 0:
+            return points
+
         reshaped_points = points.reshape(-1, 1, 2).astype(np.float32)
         transformed_points = cv2.perspectiveTransform(reshaped_points, self.m)
         return transformed_points.reshape(-1, 2)
