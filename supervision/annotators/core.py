45fe3c1c0f0250688464d54b6391f5bcd6616d1c
SkalskiP <piotr.skalski92@gmail.com>
2024-01-23 12:13:08 +0100

    small bug fix

--- supervision/annotators/core.py
+++ supervision/annotators/core.py
@@ -1593,7 +1593,7 @@ class PercentageBarAnnotator(BaseAnnotator):
 
             value = (
                 custom_values[detection_idx]
-                if custom_values
+                if custom_values is not None
                 else detections.confidence[detection_idx]
             )
 
