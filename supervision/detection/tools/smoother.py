e2751368d573290f30308cf94af4c012580d016b
SkalskiP <piotr.skalski92@gmail.com>
2024-01-24 13:06:07 +0100

    improvements

--- supervision/detection/tools/smoother.py
+++ supervision/detection/tools/smoother.py
@@ -1,4 +1,5 @@
-from collections import deque
+from collections import deque, defaultdict
+from copy import deepcopy
 from typing import Optional
 
 import numpy as np
@@ -8,132 +9,77 @@ from supervision.detection.core import Detections
 
 class DetectionsSmoother:
     """
-    Smooth out noise in predictions over time with the `DetectionsSmoother` class.
-    This classes uses an existing `Tracker` to track objects over time.
-    Detections are averaged out over the `length` most recent frames.
-
-    <video controls>
-        <source
-            src="https://media.roboflow.com/supervision/video-examples/smoothed-grocery-example-720.mp4"
-            type="video/mp4">
-    </video>
-    > _On the left are the model's raw predictions,
-    > on the right is the output of DetectionsSmoother._
+    A utility class for smoothing detections over multiple frames in video tracking.
+    It maintains a history of detections for each track and provides smoothed
+    predictions based on these histories.
 
     !!! warning
 
-        DetectionsSmoother uses the `tracker_id`. Read
-        [here](https://supervision.roboflow.com/trackers/) to learn
-        how to plug tracking into your inference pipeline.
+        - `DetectionsSmoother` requires the `tracker_id` for each detection. Refer to
+          [Roboflow Trackers](https://supervision.roboflow.com/trackers/) for
+          information on integrating tracking into your inference pipeline.
+        - This class is not compatible with segmentation models.
 
-        Note: DetectionsSmoother is intended for use on Detections
-        without a `mask` field.
+    Example:
+        ```python
+        import supervision as sv
+        from ultralytics import YOLO
 
-    ## Example Usage:
+        video_info = sv.VideoInfo.from_video_path(video_path=<SOURCE_FILE_PATH>)
+        frame_generator = sv.get_video_frames_generator(source_path=<SOURCE_FILE_PATH>)
 
-    ```python
-    import cv2
-    # remember to `pip install inference`
-    from inference import InferencePipeline
-    import supervision as sv
+        model = YOLO(<MODEL_PATH>)
+        tracker = sv.ByteTrack(frame_rate=video_info.fps)
+        smoother = sv.DetectionsSmoother()
 
-    box_annotator = sv.BoxAnnotator(color=sv.Color(52, 236, 217))
-    byte_tracker = sv.ByteTrack()
+        annotator = sv.BoundingBoxAnnotator()
 
-    # Initialize the Smoother
-    smoother = sv.DetectionsSmoother()
+        with sv.VideoSink(<TARGET_FILE_PATH>, video_info=video_info) as sink:
+            for frame in frame_generator:
+                result = model(frame)[0]
+                detections = sv.Detections.from_ultralytics(result)
+                detections = tracker.update_with_detections(detections)
+                detections = tracker.update_with_detections(detections)
 
-    def render(detections, video_frame):
-        # Parse the detections
-        detections = sv.Detections.from_roboflow(detections)
-
-        # Run a tracker to link predictions across frames
-        detections = byte_tracker.update_with_detections(detections)
-
-        # Record the new frame and get the smoothed predictions
-        smoothed_detections = smoother.update_with_detections(detections)
-
-        # Render
-        image_smoothed = box_annotator.annotate(
-            scene=image.copy(),
-            detections=smoothed_detections
-        )
-
-        # Visualize
-        cv2.imshow("Prediction", image)
-        cv2.waitKey(1)
-
-
-    pipeline = InferencePipeline.init(
-        model_id="microsoft-coco/9", # Or put your custom trained model here
-        # api_key="YOUR_ROBOFLOW_KEY", # Uncomment and fill if you want to access a
-        #                                model that requires auth (or setup a .env file)
-        video_reference=0, # Webcam; can also be video path or RTSP stream
-        on_prediction=render
-    )
-    pipeline.start()
-    pipeline.join()
-    ```
-    """
+                annotated_frame = bounding_box_annotator.annotate(frame.copy(), detections)
+                sink.write_frame(annotated_frame)
+        ```
+    """ # noqa: E501 // docs
 
     def __init__(self, length: int = 5) -> None:
         """
         Args:
-            length (int): The current count of detected objects within the zone
+            length (int): The maximum number of frames to consider for smoothing
+                detections. Defaults to 5.
         """
-
-        self.length = length
-
-        self.current_frame = 0
-        self.tracks = {}
-
-    def set_length(self, length: int) -> None:
-        """
-        Sets the number of frames to average out detections over.
-
-        Args:
-            length (int): The number of frames to average out detections over.
-        """
-
-        self.length = length
-        for track_id in self.tracks:
-            self.tracks[track_id] = deque(self.tracks[track_id], maxlen=length)
+        self.tracks = defaultdict(lambda: deque(maxlen=length))
 
     def update_with_detections(self, detections: Detections) -> Detections:
         """
-        Adds a new set of predictions to the smoother. Run this with every new
-        prediction received from the model.
+        Updates the smoother with a new set of detections from a frame.
 
         Args:
             detections (Detections): The detections to add to the smoother.
         """
 
-        already_tracked_ids = set(self.tracks.keys())
+        if detections.tracker_id is None:
+            print("DetectionsSmoother requires tracker_id to be set on Detections")
+            return detections
 
         for detection_idx in range(len(detections)):
             tracker_id = detections.tracker_id[detection_idx]
             if tracker_id is None:
                 continue
 
-            if self.tracks.get(tracker_id, None) is None:
-                self.tracks[tracker_id] = deque(maxlen=self.length)
+            self.tracks[tracker_id].append(detections[detection_idx])
 
-            if tracker_id in already_tracked_ids:
-                self.tracks[tracker_id].append(detections[detection_idx])
-
-        for track_id in list(self.tracks.keys()):
+        for track_id in self.tracks.keys():
             if track_id not in detections.tracker_id:
                 self.tracks[track_id].append(None)
 
         for track_id in list(self.tracks.keys()):
-            if (
-                all([d is None for d in self.tracks[track_id]])
-                and len(self.tracks[track_id]) == self.length
-            ):
+            if all([d is None for d in self.tracks[track_id]]):
                 del self.tracks[track_id]
-                print("Removed track", track_id)
-
-        print("Tracks:", self.tracks.keys())
 
         return self.get_smoothed_detections()
 
@@ -146,7 +92,7 @@ class DetectionsSmoother:
         if len(track) == 0:
             return None
 
-        ret = track.copy()[0]
+        ret = deepcopy(track[0])
         ret.xyxy = np.mean([d.xyxy for d in track], axis=0)
         ret.confidence = np.mean([d.confidence for d in track], axis=0)
 
