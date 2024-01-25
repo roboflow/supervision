cabf655042594e25145b93a6d5ee1b96cc609120
SkalskiP <piotr.skalski92@gmail.com>
2024-01-25 15:19:19 +0100

    up

--- examples/supervision_promo/supervision.0.18.0.py
+++ examples/supervision_promo/supervision.0.18.0.py
@@ -1,5 +1,6 @@
 import cv2
 import argparse
+import numpy as np
 import supervision as sv
 from ultralytics import YOLO
 
@@ -9,6 +10,16 @@ LINES_COORDINATES = [
     [sv.Point(x=468, y=381), sv.Point(x=640, y=68)],
     [sv.Point(x=1412, y=366), sv.Point(x=1286, y=62)],
 ]
+
+ZONE_COORDINATES = np.array([
+    [427, 0],
+    [0, 662],
+    [0, 1080],
+    [850, 1080],
+    [1920, 640],
+    [1920, 605],
+    [1550, 0]
+])
 
 line_zones = [
     sv.LineZone(
@@ -71,7 +82,12 @@ if __name__ == "__main__":
     thickness = 2
     text_scale = sv.calculate_dynamic_text_scale(resolution_wh=video_info.resolution_wh)
 
-    bounding_box_annotator = sv.BoundingBoxAnnotator(
+    polygon_zone = sv.PolygonZone(
+        ZONE_COORDINATES,
+        triggering_position=sv.Position.BOTTOM_CENTER,
+        frame_resolution_wh=video_info.resolution_wh)
+
+    box_corner_annotator = sv.BoxCornerAnnotator(
         thickness=thickness,
         color_lookup=sv.ColorLookup.TRACK)
     label_annotator = sv.LabelAnnotator(
@@ -84,14 +100,21 @@ if __name__ == "__main__":
         trace_length=video_info.fps * 2,
         position=sv.Position.CENTER,
         color_lookup=sv.ColorLookup.TRACK)
+    percentage_bar_annotator = sv.PercentageBarAnnotator(
+        height=20,
+        width=100,
+        border_color=sv.Color.WHITE,
+        position=sv.Position.TOP_CENTER,
+        color_lookup=sv.ColorLookup.TRACK)
+    percentage_bar_annotator.border_thickness = 2
 
     frame_generator = sv.get_video_frames_generator(source_path=args.source_video_path)
 
     with sv.VideoSink(args.target_video_path, video_info) as sink:
         for frame in frame_generator:
-            result = model(frame)[0]
+            result = model(frame, imgsz=1280)[0]
             detections = sv.Detections.from_ultralytics(result)
-
+            detections = detections[polygon_zone.trigger(detections)]
             confidence_filter = detections.confidence > args.confidence_threshold
             class_filter = (detections.class_id == 2) | (detections.class_id == 7)
 
@@ -102,19 +125,22 @@ if __name__ == "__main__":
 
             labels = [
                 f"#{tracker_id} {confidence:.2f}"
-                for tracker_id, confidence
-                in zip(detections.tracker_id, detections.confidence)
+                for tracker_id, confidence, class_id
+                in zip(detections.tracker_id, detections.confidence, detections.class_id)
             ]
 
             annotated_frame = frame.copy()
             annotated_frame = trace_annotator.annotate(
                 scene=annotated_frame, detections=detections
             )
-            annotated_frame = bounding_box_annotator.annotate(
+            annotated_frame = box_corner_annotator.annotate(
                 scene=annotated_frame, detections=detections
             )
             annotated_frame = label_annotator.annotate(
                 scene=annotated_frame, detections=detections, labels=labels
+            )
+            annotated_frame = percentage_bar_annotator.annotate(
+                scene=annotated_frame, detections=detections
             )
 
             for line_zone, line_annotation in zip(line_zones, line_annotations):
@@ -122,6 +148,13 @@ if __name__ == "__main__":
                 annotated_frame = line_annotation.annotate(
                     frame=annotated_frame, line_counter=line_zone
                 )
+
+            annotated_frame = sv.draw_polygon(
+                scene=annotated_frame,
+                polygon=ZONE_COORDINATES,
+                color=sv.Color.WHITE,
+                thickness=1,
+            )
 
             sink.write_frame(annotated_frame)
             cv2.imshow("frame", annotated_frame)
