0be32030788828d14c9b6751cf950b1d72ce6f0b
Ali-Fayzi <a.fayzi@outlook.com>
2024-01-28 18:06:22 +0330

    add load_strongsort in __init__.py

--- supervision/__init__.py
+++ supervision/__init__.py
@@ -66,6 +66,8 @@ from supervision.geometry.core import Point, Position, Rect
 from supervision.geometry.utils import get_polygon_center
 from supervision.metrics.detection import ConfusionMatrix, MeanAveragePrecision
 from supervision.tracker.byte_tracker.core import ByteTrack
+from supervision.tracker.strongsort_tracker.load_strongsort import load_strong_sort
+
 from supervision.utils.file import list_files_with_extensions
 from supervision.utils.image import ImageSink, crop_image
 from supervision.utils.notebook import plot_image, plot_images_grid
