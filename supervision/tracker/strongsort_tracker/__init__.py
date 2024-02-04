014566a6620ce2c5140be86eb9dcc9bf55e4902a
Ali-Fayzi <a.fayzi@outlook.com>
2024-02-04 22:38:21 +0330

    update __init__.py

--- supervision/tracker/strongsort_tracker/__init__.py
+++ supervision/tracker/strongsort_tracker/__init__.py
@@ -1,11 +1,11 @@
-from .strong_sort import StrongSORT
+from .strong_sort import StrongSort
 
 
-__all__ = ['StrongSORT', 'build_tracker']
+__all__ = ['StrongSort', 'build_tracker']
 
 
 def build_tracker(cfg, use_cuda):
-    return StrongSORT(cfg.STRONGSORT.REID_CKPT, 
-                max_dist=cfg.STRONGSORT.MAX_DIST, min_confidence=cfg.STRONGSORT.MIN_CONFIDENCE, 
-                nms_max_overlap=cfg.STRONGSORT.NMS_MAX_OVERLAP, max_iou_distance=cfg.STRONGSORT.MAX_IOU_DISTANCE, 
-                max_age=cfg.STRONGSORT.MAX_AGE, n_init=cfg.STRONGSORT.N_INIT, nn_budget=cfg.STRONGSORT.NN_BUDGET, use_cuda=use_cuda)
+    return StrongSort(cfg.StrongSort.REID_CKPT, 
+                max_dist=cfg.StrongSort.MAX_DIST, min_confidence=cfg.StrongSort.MIN_CONFIDENCE, 
+                nms_max_overlap=cfg.StrongSort.NMS_MAX_OVERLAP, max_iou_distance=cfg.StrongSort.MAX_IOU_DISTANCE, 
+                max_age=cfg.StrongSort.MAX_AGE, n_init=cfg.StrongSort.N_INIT, nn_budget=cfg.StrongSort.NN_BUDGET, use_cuda=use_cuda)
