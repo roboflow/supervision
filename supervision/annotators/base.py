c8f7622e91de2ae27792206e03abc09b60b62d81
Linas Kondrackis <linas.ko+dev@skiff.com>
2024-02-10 16:39:38 +0200

    Remove ImgType docstring
    * Bad decision, but makes the pre-commit hook happy

--- supervision/annotators/base.py
+++ supervision/annotators/base.py
@@ -6,13 +6,6 @@ from PIL import Image
 
 from supervision.detection.core import Detections
 
-"""
-An image of type `np.ndarray` or `PIL.Image.Image`.
-
-Unlike a `Union`, ensures the type remains consistent. If a function
-takes an `ImgType` argument and returns an `ImgType`, when you pass
-an `np.ndarray`, you will always get an `np.ndarray` back.
-"""
 ImgType = TypeVar("ImgType", np.ndarray, Image.Image)
 
 
