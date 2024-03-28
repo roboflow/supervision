767f9a71482bb94b46a8ee7da8e91f64342a84c7
jeslinpjames <jeslinpjames007@gmail.com>
2024-03-29 01:14:10 +0530

    formatted code

--- supervision/detection/core.py
+++ supervision/detection/core.py
@@ -429,8 +429,10 @@ class Detections:
         """  # noqa: E501 // docs
 
         if "boxes" in transformers_results:
-            class_ids = transformers_results["labels"].cpu().detach().numpy().astype(int)
-            data ={}
+            class_ids = (
+                transformers_results["labels"].cpu().detach().numpy().astype(int)
+            )
+            data = {}
             if id2label is not None:
                 class_names = np.array([id2label[class_id] for class_id in class_ids])
                 data[CLASS_NAME_DATA_FIELD] = class_names
@@ -438,7 +440,7 @@ class Detections:
                 xyxy=transformers_results["boxes"].cpu().detach().numpy(),
                 confidence=transformers_results["scores"].cpu().detach().numpy(),
                 class_id=class_ids,
-                data = data
+                data=data,
             )
         masks = transformers_results["masks"].cpu().detach().numpy().astype(bool)
         return cls(
