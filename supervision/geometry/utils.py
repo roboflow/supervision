7ded0bd49329af30558025637097730d9db72714
sharingan000 <sharingan202@yandex.ru>
2024-04-02 16:55:03 +1000

    Change the method of finding polygon center and create test for new function

--- supervision/geometry/utils.py
+++ supervision/geometry/utils.py
@@ -9,8 +9,10 @@ def get_polygon_center(polygon: np.ndarray) -> Point:
 
     This function takes in a polygon as a 2-dimensional numpy ndarray and
     returns the center of the polygon as a Point object.
-    The center is calculated as the mean of the polygon's vertices along each axis,
-    and is rounded down to the nearest integer.
+    
+    The center is calculated as center of frame.
+    polygon -> polygon, where p[i] = p[i + 1] + p[i] / 2,
+     with mass = length of vector p[i + 1] - p[i]
 
     Parameters:
         polygon (np.ndarray): A 2-dimensional numpy ndarray representing the
@@ -25,10 +27,15 @@ def get_polygon_center(polygon: np.ndarray) -> Point:
         from supervision.geometry.utils import get_polygon_center
         import numpy as np
 
-        vertices = np.array([[0, 0], [0, 1], [1, 1], [1, 0]])
-        get_center(vertices)
-        Point(x=0.5, y=0.5)
+        vertices = np.array([[0, 0], [0, 2], [2, 2], [2, 0]])
+        get_polygon_center(vertices)
+        Point(x=1, y=1)
         ```
     """
-    center = np.mean(polygon, axis=0).astype(int)
+    shifted_polygon = np.roll(polygon, 1, axis=0)
+    points = (shifted_polygon + polygon) / 2
+    vectors = shifted_polygon - polygon
+    mass = (vectors[:, 0] ** 2 + vectors[:, 1] ** 2) ** 0.5
+    mass = np.array([mass, mass]).T
+    center = (np.sum(points * mass, axis=0) / np.sum(mass) * 2).round()
     return Point(x=center[0], y=center[1])
