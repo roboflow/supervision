6913ecf54d59ba270c2efa69586c15e9ac5729fb
magda skoczen <m.sko@o2.pl>
2024-05-03 01:19:01 +0200

    2 annotations with segmentation, one polygon one RLE

--- test/dataset/formats/test_coco.py
+++ test/dataset/formats/test_coco.py
@@ -263,9 +263,38 @@ def test_group_coco_annotations_by_image_id(
                 mask = np.array([0 if i>=10 or j>=10 or (i<5 and j >=5)  else 1 for i in range(0,20) for j in range(0,20)]).reshape((1,20,20))
             ),
             DoesNotRaise(),
-        ),  # single image annotations with mask, RLE encoded segmentation mask in L-like shape, like below:
+        ),  # single image annotations with mask, RLE segmentation mask in L-like shape, like below:
             #                                     1 0 0 0
             #                                     1 1 0 0 
+            #                                     0 0 0 0 
+            #                                     0 0 0 0 
+
+        (
+            [
+                mock_cock_coco_annotation(
+                    category_id=0, bbox=(0, 0, 10, 10), area=10 * 10, segmentation = [[0,0, 4,0, 4,5, 9,5,  9,9, 0,9]]
+                ),
+                mock_cock_coco_annotation(
+                    category_id=0, bbox=(5, 0, 5, 5), area=5 * 5, 
+                    segmentation = {'size':[20,20], 'counts':[100, 5, 15, 5, 15, 5, 15, 5, 15, 5, 215]}, iscrowd = True 
+                ),
+            ],
+            (20, 20),
+            True,
+            Detections(
+                xyxy=np.array(
+                    [[0, 0, 10, 10], [5, 0, 10, 5]], dtype=np.float32
+                ),
+                class_id=np.array([0, 0], dtype=int),
+                mask = np.array([
+                    np.array([0 if i>=10 or j>=10 or (i<5 and j >=5)  else 1 for i in range(0,20) for j in range(0,20)]).reshape((20,20)),
+                    np.array([1 if j>4 and j<10 and i<5  else 0 for i in range(0,20) for j in range(0,20)]).reshape((20,20))
+                    ])
+            ),
+            DoesNotRaise(),
+        ),  # two image annotations with mask, one mask as polygon in in L-like shape, second as RLE in shape of square, like below (P = polygon, R = RLE):
+            #                                     P R 0 0
+            #                                     P P 0 0 
             #                                     0 0 0 0 
             #                                     0 0 0 0 
 
