0cd2c9d1464d2de77db97b7fc5550d05955477eb
magda skoczen <m.sko@o2.pl>
2024-05-17 11:16:37 +0200

    documentation change for as_coco

--- supervision/dataset/core.py
+++ supervision/dataset/core.py
@@ -430,6 +430,10 @@ class DetectionDataset(BaseDataset):
         """
         Exports the dataset to COCO format. This method saves the
         images and their corresponding annotations in COCO format.
+        The format of the mask is determined automatically: 
+        when a mask consists of multiple disconnected elements 
+        or has holes the RLE format is used,
+        otherwise, the mask is encoded as a polygon.
 
         Args:
             images_directory_path (Optional[str]): The path to the directory
