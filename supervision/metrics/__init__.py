6bb5929e0922cf8e790132c7654bf74a579ac99b
LinasKo <linkondrackis@gmail.com>
2024-08-28 20:12:04 +0300

    Remove IoU metric for now - unclear external API

--- supervision/metrics/__init__.py
+++ supervision/metrics/__init__.py
@@ -1,8 +1,4 @@
 from supervision.metrics.core import CLASS_ID_NONE, Metric, MetricTarget
-from supervision.metrics.intersection_over_union import (
-    IntersectionOverUnion,
-    IntersectionOverUnionResult,
-)
 from supervision.metrics.mean_average_precision import (
     MeanAveragePrecision,
     MeanAveragePrecisionResult,
