3cf96349071b5459ed2bc4dd366e9c16ca639e1b
LinasKo <linkondrackis@gmail.com>
2024-09-16 15:14:40 +0300

    WIP: F1 score

--- supervision/metrics/__init__.py
+++ supervision/metrics/__init__.py
@@ -3,6 +3,7 @@ from supervision.metrics.mean_average_precision import (
     MeanAveragePrecision,
     MeanAveragePrecisionResult,
 )
+from supervision.metrics.f1_score import F1Score, F1ScoreResult
 from supervision.metrics.utils.object_size import (
     ObjectSizeCategory,
     get_detection_size_category,
