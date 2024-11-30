28fa054fdcd84587d2c37ad8f893f98a489731b7
Soham <kalburgisoham@gmail.com>
2024-11-30 13:56:18 +0530

    fix: is_empty check for filtering

--- supervision/detection/core.py
+++ supervision/detection/core.py
@@ -1203,6 +1203,8 @@ class Detections:
             return self.data.get(index)
         if isinstance(index, int):
             index = [index]
+        if self.is_empty():
+            return Detections.empty()
         return Detections(
             xyxy=self.xyxy[index],
             mask=self.mask[index] if self.mask is not None else None,
