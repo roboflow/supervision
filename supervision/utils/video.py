941700b9f82d1c3873b7f43e13d7dee208d180ab
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2025-04-07 21:42:35 +0530

    update: supervision/utils/video.py
    Co-authored-by: Onuralp SEZER <thunderbirdtr@gmail.com>

--- supervision/utils/video.py
+++ supervision/utils/video.py
@@ -234,14 +234,13 @@ def process_video(
         source_path=source_path, end=max_frames
     )
     with VideoSink(target_path=target_path, video_info=source_video_info) as sink:
-        video_frames_generator = get_video_frames_generator(
-            source_path=source_path, end=max_frames
-        )
-        total_frames = (
-            min(source_video_info.total_frames, max_frames)
-            if max_frames is not None
-            else source_video_info.total_frames
-        )
+        if show_progress:
+            # Calculate total_frames only when needed for tqdm
+            total_frames = (
+                min(source_video_info.total_frames, max_frames)
+                if max_frames is not None
+                else source_video_info.total_frames
+            )
         for index, frame in enumerate(
             tqdm(
                 video_frames_generator,
