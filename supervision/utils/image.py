ccf84d5653c172b94d66619ceca5b8b8b196d731
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2025-07-08 16:21:08 +0530

    update: image.py

--- supervision/utils/image.py
+++ supervision/utils/image.py
@@ -785,4 +785,4 @@ def _merge_tiles_elements(
 def _generate_color_image(
     shape: Tuple[int, int], color: Tuple[int, int, int]
 ) -> np.ndarray:
-    return np.ones(shape[::-1] + (3,), dtype=np.uint8) * color  # noqa RUF005
+    return np.ones(shape[::-1] + (3,), dtype=np.uint8) * color
