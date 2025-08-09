29fb6665dc593cc05b894bb7d3fec443a6b3e77e
Ashp116 <ashp116@outlook.com>
2025-08-09 01:51:08 -0400

    FIX: Fixed futuer annotations

--- supervision/video/core.py
+++ supervision/video/core.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from collections.abc import Callable
 
 import numpy as np
--- supervision/video/utils.py
+++ supervision/video/utils.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from dataclasses import dataclass
 from enum import Enum
 
