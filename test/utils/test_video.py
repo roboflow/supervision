cc7fe675f44d12198c935c6ea05d32ab8cfaf7e5
jirka <6035284+Borda@users.noreply.github.com>
2026-01-26 16:21:56 +0100

    Rename `create_test_video` to `_create_test_video` in `test_video.py` to indicate private use.

--- test/utils/test_video.py
+++ test/utils/test_video.py
@@ -4,7 +4,7 @@ import numpy as np
 from supervision.utils.video import process_video
 
 
-def create_test_video(path, num_frames, width=20, height=10):
+def _create_test_video(path, num_frames, width=20, height=10):
     fourcc = cv2.VideoWriter_fourcc(*"mp4v")
     out = cv2.VideoWriter(path, fourcc, 1.0, (width, height))
 
@@ -19,7 +19,7 @@ def test_process_video_max_frames_exceeds_total_frames(tmp_path):
     source_path = tmp_path / "source.mp4"
     target_path = tmp_path / "target.mp4"
 
-    create_test_video(str(source_path), num_frames=5)
+    _create_test_video(str(source_path), num_frames=5)
 
     process_video(
         source_path=str(source_path),
